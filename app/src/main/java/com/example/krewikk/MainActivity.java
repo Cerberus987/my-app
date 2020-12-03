e1ee7623c3e5a73b93f05a13bd091c9a04cd4797
dima mchedlidze <49157679+Cerberus987@users.noreply.github.com>
2020-12-04 01:49:31 +0400

    .

---
+++ app/src/main/java/com/example/krewikk/MainActivity.java
@@ -0,0 +1,9 @@
+package com.example.krewikk;
+
+import android.app.Activity;
+import android.view.View;
+
+public class MainActivity extends Activity {
+    public void resetGame(View view) {
+    }
+}
